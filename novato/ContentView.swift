57128f606e0fa6e98a013af859efbf9ee58b43ca
fatherdougalmaguire <144415904+fatherdougalmaguire@users.noreply.github.com>
2025-07-22 18:50:30 +1000

    First push

---
+++ novato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  novato
+//
+//  Created by Antonio Sanchez-Rivas on 22/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Eventually this will be a Microbee emulator")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ novato/novatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  novatoApp.swift
+//  novato
+//
+//  Created by Antonio Sanchez-Rivas on 22/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct novatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
